49d6c02dadaf35deda09ed5a514d744dae762d90
Ira Kovalchuk <ira021995@gmail.com>
2026-09-21 02:05:49 +0300

    feat: lesson's task done

--- task1.ts
+++ task1.ts
@@ -1,8 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const name: string = "Александр";
 
 const text: string = `Я, ${name}, учу TYPESCRIPT`;
 
-//Написать программу, которая выводит text в нижнем регистре с вашим именем, если имя не совпадает, то заменить его
+// Написать программу, которая выводит text в нижнем
+// регистре с вашим именем, если имя не совпадает,
+// то заменить его
 
 const yourName: string = "Настя";
+
+if (text.includes(yourName)) {
+  console.log(text.toLowerCase());
+} else {
+  const firstNameIndex = text.indexOf(name);
+  const lastNameIndex = firstNameIndex + name.length;
+  const lastNameAfterName = text.slice(lastNameIndex);
+
+  const newText: string = text.slice(0, firstNameIndex).concat(yourName).concat(lastNameAfterName);
+
+  console.log(newText.toLowerCase()); // тоже исправил toLocaleLowerCase → toLowerCase
+}
--- task2.ts
+++ task2.ts
@@ -1,7 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const num1: number = 0.7767667;
-const num2: number = 3.2232332;
+const num1: number = 7.9078;
+const num2: number = 2.09754;
 
-//Написать программу, которая выведет целочисленную сумму двух чисел, для оптимизации - сделать проверку на целое число
+//Написать программу, которая выведет целочисленную сумму двух чисел,
+// для оптимизации - сделать проверку на целое число
 
 const newNum = num1 + num2;
+
+console.log(Number.isInteger(newNum) ? newNum : Math.round(newNum));
